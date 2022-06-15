02944d457b50a595bc2f3cf08a5763c9ec15070a
yuxiio <2468108646@qq.com>
2022-06-15 21:59:38 +0800

    添加服务器端邮件提醒功能

--- linux服务器/start.py
+++ linux服务器/start.py
@@ -1,5 +1,7 @@
 from windows本地.mainwork import mainWork
 from windows本地.localLog import saveLogAsTXT
+import email_alert
 
 if __name__ == "__main__":
     nameDict = mainWork()
+    email_alert.if_send(nameDict)
